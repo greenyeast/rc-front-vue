1b43dbf56f15ab47b5cdc8e449b964cd54c819df
SionLee21 <sionl1030@gmail.com>
2024-01-16 21:08:40 +0900

    Style: Add union components and Union.vue

--- src/components/union/Card.vue
+++ src/components/union/Card.vue
@@ -1,13 +1,47 @@
 <template>
+  <div class="card shadow-sm">
+    <span class="img" :style="{backgroundImage: `url(${item.imgPath})`}"/>
+    <div class="card-body">
+      <p class="card-text"><span>{{ item.name }} &nbsp;</span>
+        <span class="discount badge bg-danger">
+          {{ item.discountPer }}%
+        </span></p>
+      <div class="d-flex justify-content-between align-items-center">
+        <button class="btn btn-primary">구입하기</button>
+        <small class="price text-muted">{{ lib.getNumberFormatted(item.price) }}원</small>
 
+        <small class="real text-danger">
+          {{ lib.getNumberFormatted(item.price - (item.price * item.discountPer / 100)) }}%
+        </small>
+      </div>
+    </div>
+  </div>
 </template>
-
 <script>
+import lib from "@/scripts/lib"
+
 export default {
-  name: "Card.vue"
+  name: "Card",
+  props: {
+    item: Object
+  },
+  setup() {
+    return {lib}
+  }
 }
 </script>
 
 <style scoped>
+.card .img {
+  display: inline-block;
+  width: 100%;
+  height: 250px;
+  background-size: cover;
+  background-position: center;
 
+}
+
+.card .card-body .price {
+  text-decoration: line-through;
+}
 </style>
---
+++ src/components/union/LogoCard.vue
@@ -0,0 +1,61 @@
+<!-- LogoCard.vue -->
+
+<template>
+  <div class="logo-card">
+    <img :src="cardSrc" :alt="rcName + ' card-logo'">
+    <span class="rc-name">{{ rcName }}</span>
+    <div class="num-waiting-teams">
+      <span class="num-waiting">{{ numWaiting }}</span>
+      <span class="team-text"> teams</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    cardSrc: String,
+    rcName: String,
+    numWaiting: Number,
+  },
+};
+</script>
+
+<style scoped>
+.logo-card {
+  width: 140px;
+  height: 299.2px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  text-align: center;
+}
+
+.logo-card img {
+  width: 90px;
+  height: 171.2px;
+}
+
+.rc-name {
+  font-size: 20px;
+  font-weight: 400;
+  line-height: 28px;
+  gap: 8px;
+}
+
+.num-waiting-teams {
+  height: 36px;
+  font-size: 28px;
+  line-height: 36px;
+  letter-spacing: 0em;
+}
+
+.num-waiting {
+  font-weight: 700;
+}
+
+.team-text {
+  font-weight: 400;
+}
+</style>
